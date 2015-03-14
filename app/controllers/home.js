77dfe4426f194486449e713843a17ea5fa832462
torreswoo <dew1405@hanmail.net>
2015-03-14 16:44:03 +0900

    ver1.3

--- app/controllers/home.js
+++ app/controllers/home.js
@@ -2,6 +2,10 @@ var express = require('express'),
 	router = express.Router(),
   	Article = require('../models/article');
 
+//var bodyParser = require('body-parser');
+
+//app.use(express.bodyParser());
+
 //------------- MySQL-----------------//
 var mysql = require('mysql');
 
@@ -26,8 +30,7 @@ router.get('/', function (req, res, next) {
     });
 });
 
-
-//hyperlink to Login page
+//hyperlink to Login page//////////////////////
 router.get('/loginform', function (req, res, next) {
 
   	var articles = [new Article(), new Article()];
@@ -41,148 +44,66 @@ router.get('/loginform', function (req, res, next) {
 router.post('/login', function (req, res, next) {
 	var articles = [new Article(), new Article()];
 
-//	var user_id = req.params('user_id');
-	// user_cd로 DB에서 SELECT수핼
-	// var sql = 'select * from User where User_Id=?';
-	// var query = connection.query(sql, [ user_id ], 
-	// 	function (err, rows) {
-	//     if(err){
-	//         connection.release();
-	//         throw err;
-	//     }
-	//     console.log(rows);
+	var user_id = req.body.user_id;
+	var user_pwd = req.body.user_pwd;
 
-	// });
-	// console.log(rows[0]);
-//	console.log(user_id);
+	// DB 에 정보저장 및 예외처리수행
+	console.log("user_id : "+user_id);
+	console.log("user_pwd : "+user_pwd);
 
-//	if( user_id == 'next'){
-		console.log('equal id');
-		res.render('main', {
-	       	title: 'Generator-Express MVC',
-	       	articles: articles
-	    });
-//	}
-
- 	// res.render('main', {
-  //     title: 'Generator-Express MVC',
-  //     articles: articles
-  //   });
-
+	res.render('main', {
+	   	title: 'Generator-Express MVC',
+	   	articles: articles
+	});
 });
+///////////////////////////////////////////////
 
 
-
-//hyperlink to Signup page
-router.get('/signup', function (req, res, next) {
-  	var articles = [new Article(), new Article()];
-
+//hyperlink to Signup page/////////////////////
+// 회원가입 Page
+router.get('/signupform', function (req, res, next) {
+	var articles = [new Article(), new Article()];
     res.render('signup', {
       title: 'Generator-Express MVC',
       articles: articles
     });
 });
 
-//hyperlink to Login page
+router.post('/signup', function (req, res, next) {
+  	var articles = [new Article(), new Article()];
+
+  	var user_id = req.body.user_id;
+	var user_pwd = req.body.user_pwd;
+
+	// DB 에 정보저장 및 예외처리수행
+	console.log("user_id : "+user_id);
+	console.log("user_pwd : "+user_pwd);
+
+    res.render('main', {
+      title: 'Generator-Express MVC',
+      articles: articles
+    });
+});
+
+///////////////////////////////////////////////
+
+
+//hyperlink to Main page ///////////////////////
 router.get('/main', function (req, res, next) {
-//	console.log('start /main');
-//  	res.render('index', { title: 'Express',name:'Terry' });
- // 	var user_id = req.param('user_id');
+
   	var articles = [new Article(), new Article()];
 
   	console.log('start /main');
-
- // 	if( user_id == 'next'){
-//		console.log('equal id');
-	    res.render('main', {
-	      title: 'Generator-Express MVC',
-	      articles: articles
-	    });
-//	}
+    res.render('main', {
+	    title: 'Generator-Express MVC',
+	    articles: articles
+	});
 });
 
 
 
-//추가된 router
-router.get('/users', function (req, res, next) {
-	var name = req.param('name');
-	var price = req.param('price');
-  	res.send('name : '+name + ', price: ' + price);// 받은 값을 다시 넘겨주기
-});
 
-router.post('/users', function (req, res, next) {
-	var name = req.param('name');
-	var price = req.param('price');
-	var item={
-		name: name,
-		price: price
-	};
-
-	// //// select 
-	// var query = connection.query('select * from User', function (err, rows) {
-	//     if(err){
-	//         connection.release();
-	//         throw err;
-	//     }
-	//     console.log(rows);
-	// });
-	// /////
-
-  	res.send({ 
-  		message:'데이터를 추가했습니다.', 
-  		data: item
-  	});
-});
-
-
-// DB 접근이 필요한 router
-// 1. select
-// router.get('/login', function (req, res, next) {
-// 	var articles = [new Article(), new Article()];
-
-// 	var user_id = req.param('user_id');
-// 	// user_cd로 DB에서 SELECT수핼
-// 	var sql = 'select * from User where User_Id=?';
-// 	var query = connection.query(sql, [ user_id ], 
-// 		function (err, rows) {
-// 	    if(err){
-// 	        connection.release();
-// 	        throw err;
-// 	    }
-// 	    console.log(rows);
-
-// 	});
-// 	console.log(rows[0]);
-
-// //	res.redirect('/main');
-//  	res.render('main', {
-//       title: 'Generator-Express MVC',
-//       articles: articles
-//     });
-
-// });
-
-// 2. insert
-router.post('/register', function (req, res, next) {
-	var user_id = req.param('user_id');
-	var user_pwd = req.param('user_pwd');
-	var user_type_cd = req.param('user_type_cd');
-
-	console.log('start /register');
-	//// select 
-	// var sql = 'insert into User(user_id, user_pwd, user_type_cd) VALUES(?, ?, ?)';
-	// var query = connection.query(sql, [user_id, user_pwd, user_type_cd], 
-	// 	function (err, rows) {
-	//     if(err){
-	//         connection.release();
-	//         throw err;
-	//     }
-	//     console.log(rows);
- //  	});
-//  	res.redirect('login');
-  	res.redirect('/main');
-});
-
+///////////////////////////////////////////////
 // 3. delete
 router.get('/dbDelete', function (req, res, next) {
 	var user_cd = req.param('user_cd');
@@ -202,3 +123,42 @@ router.get('/dbDelete', function (req, res, next) {
 	});
 
 });
+
+
+// INSERT
+// var sql = 'insert into User(user_id, user_pwd, user_type_cd) VALUES(?, ?, ?)';
+// var query = connection.query(sql, [user_id, user_pwd, user_type_cd], 
+// 	function (err, rows) {
+//     if(err){
+//         connection.release();
+//         throw err;
+//     }
+//     console.log(rows);
+//  	});
+
+// DELETE
+// var sql = 'delete from User where User_Cd=?';
+// var query = connection.query(sql, [ user_cd ], 
+// 	function (err, rows) {
+//     if(err){
+//         connection.release();
+//         throw err;
+// 	//	res.send('delete fail!');//처리해주어야..
+//     }
+// //    console.log(rows);
+// 	res.send('delete success!');
+// });
+
+
+// SELECT
+// 	var user_id = req.param('user_id');
+// 	var sql = 'select * from User where User_Id=?';
+// 	var query = connection.query(sql, [ user_id ], 
+// 		function (err, rows) {
+// 	    if(err){
+// 	        connection.release();
+// 	        throw err;
+// 	    }
+// 	    console.log(rows);
+// 	});
+
--- public/js/DB_manager.js
+++ public/js/DB_manager.js
@@ -1,23 +1,6 @@
 
 $(document).ready(function(){
 
-	// DB select
-	// $('#submit').click(function(){  // id
-	// 	var user_id = $('#user_id').val();// 값을 가져오기
-	// 	var user_pwd = $('#user_pwd').val();
-	// 	$.ajax({
-	// 		url: '/login?user_id='+user_id,//GET방식으로 URL에추가하기
-	// 		type: 'get', 
-	// 		dataType:'text';
-
-	// 		success: function(data){
-	// 			console.log("success");
-	// 		}
-	// 	});
-	// 	console.log('end SELECT request');
-	// });
-
-
 	// $('#login').click(function(){  // id
 	// 	console.log("user_id: "+ $('#user_id').val() + ",user_pwd:"+$('#user_pwd').val());
 	// 	$.ajax({
@@ -37,24 +20,24 @@ $(document).ready(function(){
 	// });
 
 	// Sign up button listener
-	$('#signup').click(function(){	
-		$.ajax({
-			url: '/register',
-			type: 'post', 
-			dataType:'text', 
-			data:{
-				user_id: $('#user_id').val(),
-				user_pwd: $('#user_pwd').val(),
-				user_type_cd: $('#user_type_cd').val()
-			},
-		//	console.log('data sent');
+	// $('#signup').click(function(){	
+	// 	$.ajax({
+	// 		url: '/register',
+	// 		type: 'post', 
+	// 		dataType:'text', 
+	// 		data:{
+	// 			user_id: $('#user_id').val(),
+	// 			user_pwd: $('#user_pwd').val(),
+	// 			user_type_cd: $('#user_type_cd').val()
+	// 		},
+	// 	//	console.log('data sent');
 
-			success: function(data){
-				console.log('success');
-			}
-		});
-		console.log('end POST request');
-	});	
+	// 		success: function(data){
+	// 			console.log('success');
+	// 		}
+	// 	});
+	// 	console.log('end POST request');
+	// });	
 
 	// DB select
 	$('#db_delete').click(function(){  // id
